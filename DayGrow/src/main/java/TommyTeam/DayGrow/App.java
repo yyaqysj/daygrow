331d07100689648db1932ffe8bff74bf85546e5f
tommy <tommy_li@daygrow.top>
2017-04-17 20:11:46 +0800

    init daygrow project to maven project

---
+++ DayGrow/src/main/java/TommyTeam/DayGrow/App.java
@@ -0,0 +1,13 @@
+package TommyTeam.DayGrow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
